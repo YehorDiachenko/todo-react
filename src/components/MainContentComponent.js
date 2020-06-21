8138215fec08fcb2de01cc38a38aa07c1edc7578
YehorDiachenko <diachenko.yehor@gmail.com>
2020-06-21 21:40:58 +0300

    Basics of styling

---
+++ src/components/MainContentComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MainContent() {
+    return (
+        <div>
+          <input type="checkbox" /><p>Wake up</p>
+          <input type="checkbox" /><p>Wash face</p>
+          <input type="checkbox" /><p>Brush teeth</p>
+          <input type="checkbox" /><p>Have a breakfast</p>
+        </div>
+    )
+}
+
+export default MainContent;
