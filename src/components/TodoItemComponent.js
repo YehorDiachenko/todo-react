39f956c529422edc8cbc39a73fa6f6a10a8a40f4
YehorDiachenko <diachenko.yehor@gmail.com>
2020-06-30 13:33:06 +0300

    Conditional Rendering

--- src/components/TodoItemComponent.js
+++ src/components/TodoItemComponent.js
@@ -1,6 +1,12 @@
 import React from "react"
 
 function TodoItem(props) {
+  const itemCompleted = {
+    color: "#666666",
+    textDecoration: "line-through",
+    fontStyle: "italic"
+  };
+
     return (
         <div className="todo-item">
             <input
@@ -8,7 +14,7 @@ function TodoItem(props) {
                 checked={props.item.completed}
                 onChange={() => props.handleChange(props.item.id)}
             />
-            <p>{props.item.text}</p>
+            <p style={props.item.completed ? itemCompleted : null}>{props.item.text}</p>
         </div>
     )
 }
